c67ea32cf1d04b84133a267dfd4f3bacfdaac06f
GurkiratS22 <143457348+GurkiratS22@users.noreply.github.com>
2024-03-19 23:19:55 -0700

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Gurkirat Singh &nbsp;
-            <span className='sm:block hidden'> | Portfolio</span>
+            Adrian &nbsp;
+            <span className='sm:block hidden'> | JavaScript Mastery</span>
           </p>
         </Link>
 
