8cd8a39cb0a4042e433189dc15b0b98fe99a387e
GurkiratS22 <143457348+GurkiratS22@users.noreply.github.com>
2024-03-23 02:36:17 -0700

    Progress 7

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
              initial={{opacity: 0, scale: 0.5}}
              animate={{ opacity: 1, scale: 1}}
              transition={{ duration: 0.5 }}
-             >Gurkirat Singh</motion.span>
+             >Gurkirat Singh | Portfolio</motion.span>
             <div className="social">
                 <a href="#"><img src="./facebook.png" alt="" /></a>
                 <a href="#"><img src="./instagram.png" alt="" /></a>
